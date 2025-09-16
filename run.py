e590a1a0a84541befa14c1c6375f5a086d15f551
jit singha <jitsingha570@gmail.com>
2025-09-16 23:05:06 +0530

    Initial commit: Add Rasa chatbot project

--- run.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-import subprocess
-
-# Load environment variables
-load_dotenv()
-
-# Run rasa
-subprocess.run(["rasa", "run", "--enable-api", "--cors", "*"])
