7c51406c4e6981d276fb25f90c0e36d0f8a1dd91
jit singha <jitsingha570@gmail.com>
2025-09-18 20:19:16 +0530

    twillo connect

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,31 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from twilio.rest import Client
+import os
+
+class ActionSendWhatsApp(Action):
+    def name(self):
+        return "action_send_whatsapp"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: dict):
+
+        # Twilio credentials from environment
+        account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+        auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+        from_whatsapp = os.getenv("TWILIO_PHONE_NUMBER")
+        to_whatsapp = tracker.get_slot("user_whatsapp_number")  # you can store user's number in slot
+
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body="Hello from Rasa WhatsApp bot!",
+            from_=f'whatsapp:{from_whatsapp}',
+            to=f'whatsapp:{to_whatsapp}'
+        )
+
+        dispatcher.utter_message(text="Message sent on WhatsApp!")
+        return []
